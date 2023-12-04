548a918e438e521d75c2d050f16740334b6d7657
Kyroline <rizky.w.dewantoro@gmail.com>
2023-12-04 20:05:22 +0700

    Minor refactoring

--- controllers/course/student/getAll/repository.go
+++ controllers/course/student/getAll/repository.go
@@ -7,7 +7,7 @@ import (
 )
 
 type Repository interface {
-	GetAllCourseRepository(StudentID string, ClassID string) (*[]model.Course, string)
+	GetAllCourseRepository(StudentID string, ClassID string) (*[]model.Enrollment, string)
 }
 
 type repository struct {
@@ -18,18 +18,21 @@ func NewGetAllCourseRepository(db *gorm.DB) *repository {
 	return &repository{db: db}
 }
 
-func (r *repository) GetAllCourseRepository(StudentID string, ClassID string) (*[]model.Course, string) {
-	var class model.Class
-	r.db.Where("id = ?", ClassID).Find(&class)
+func (r *repository) GetAllCourseRepository(StudentID string, ClassID string) (*[]model.Enrollment, string) {
+	// var class model.Class
+	// r.db.Where("id = ?", ClassID).Find(&class)
 
-	var courses []model.Course
-	if err := r.db.Model(&class).Association("Courses").Find(&courses); err != nil {
-		return nil, "500_INTERNAL"
-	}
+	// var courses []model.Course
+	// if err := r.db.Model(&class).Association("Courses").Find(&courses); err != nil {
+	// 	return nil, "500_INTERNAL"
+	// }
 
-	if len(courses) == 0 {
-		return nil, "404_NOTFOUND"
-	}
+	// if len(courses) == 0 {
+	// 	return nil, "404_NOTFOUND"
+	// }
+
+	var courses []model.Enrollment
+	r.db.Model(model.Enrollment{}).Preload("Course").Preload("Lecturers").Where("class_id = ?", ClassID).Find(&courses)
 
 	return &courses, ""
 }
--- controllers/course/student/getAll/service.go
+++ controllers/course/student/getAll/service.go
@@ -5,7 +5,7 @@ import (
 )
 
 type Service interface {
-	GetAllCourseService(input InputGetAllCourse) (*[]model.Course, string)
+	GetAllCourseService(input InputGetAllCourse) (*[]model.Enrollment, string)
 }
 
 type service struct {
@@ -16,7 +16,7 @@ func NewGetAllCourseService(repository Repository) *service {
 	return &service{repository: repository}
 }
 
-func (s *service) GetAllCourseService(input InputGetAllCourse) (*[]model.Course, string) {
+func (s *service) GetAllCourseService(input InputGetAllCourse) (*[]model.Enrollment, string) {
 	res, err := s.repository.GetAllCourseRepository(input.StudentID, input.ClassID)
 
 	return res, err
