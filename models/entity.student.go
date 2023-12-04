d84dbcefc4160154076f2135e143e4257128cdfc
Kyroline <rizky.w.dewantoro@gmail.com>
2023-12-04 20:04:49 +0700

    Models refactoring

--- models/entity.student.go
+++ models/entity.student.go
@@ -2,8 +2,6 @@ package model
 
 import (
 	"time"
-
-	"gorm.io/gorm"
 )
 
 type Student struct {
@@ -17,11 +15,4 @@ type Student struct {
 	Address string    `gorm:"type:varchar(255)"`
 	Dob     time.Time `gorm:"type:date"`
 	Year    uint
-}
-
-func (entity *Student) AfterFind(tx *gorm.DB) (err error) {
-	format := "2006-01-02"
-	date, _ := time.Parse(format, entity.Dob.String())
-	entity.Dob = date
-	return
 }
