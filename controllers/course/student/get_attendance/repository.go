dffee73feb7134a117d1f0b3936c104296632c89
Kyroline <rizky.w.dewantoro@gmail.com>
2024-01-15 16:01:49 +0700

    Fixed bug where student can view all course regarding of their class

--- controllers/course/student/get_attendance/repository.go
+++ controllers/course/student/get_attendance/repository.go
@@ -46,6 +46,6 @@ func (r *repository) GetEnrollmentByID(student *model.Student, id string) (*mode
 
 func (r *repository) GetCourseEvent(course *model.Course, student *model.Student) (*[]model.Event, string) {
 	var events []model.Event
-	r.db.Model(events).Preload("Students", "id = ?", student.ID).Where("course_id = ?", course.ID).Find(&events)
+	r.db.Model(events).Preload("Students", "id = ?", student.ID).Where("course_id = ? AND class_id = ?", course.ID, student.ClassID).Find(&events)
 	return &events, ""
 }
